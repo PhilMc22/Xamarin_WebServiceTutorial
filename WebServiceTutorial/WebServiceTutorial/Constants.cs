c7abc54c6dff22609f26733e713d40d5e0ad790d
PhilMc22 <pmc2292@gmail.com>
2020-04-06 16:58:36 +0100

    Completed tutorial

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace WebServiceTutorial
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "2e4beab56b13ff99d0a36a9225867e3e";
+    }
+}
